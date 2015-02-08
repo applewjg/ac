83a3746973a0986bb83feb8d950593f16cc45a06
applewjg <wangjingui1988@163.com>
2015-02-08 20:34:08 +0800

    The last non zero Factorial
    Change-Id: Id3149971a87becb7fdbf48a44b08f61924da47d4

---
+++ JustTheFacts.cpp
@@ -0,0 +1,38 @@
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#define MAXN 10000
+int lastdigit(char* buf)
+{
+    /*
+     Lets say D(N) denotes the last non zero digit of factorial, then the algo says
+     D(N)=4*D[N/5]*D(Unit digit of N)[If tens digit of N is odd]
+     D(N)=6*D[N/5]*D(Unit digit of N)[If tens digit of N is even]; Where [N/5] is greatest Integer Function
+     */
+    const int mod[20]= {1,1,2,6,4,2,2,4,2,8,4,4,8,4,6,8,8,6,8,2};
+        int len=strlen(buf),a[MAXN],i,c,ret=1;
+    if (len==1)
+        return mod[buf[0]-'0']; //特殊情况当数小于10
+    for (i=0; i<len; i++)
+        a[i]=buf[len-1-i]-'0';  //字符向数字转换
+    for (; len; len-=!a[len-1])
+    {
+        ret = ret * mod[ a[1]%2*10+a[0] ] % 5;
+        for (c=0,i=len-1; i>=0; i--)
+        {
+            c=c*10+a[i];
+            a[i]=c/5;
+            c%=5;
+        }
+    }
+    return ret+ret%2*5;
+}
+int main()
+{
+    char a[10000];
+    while(scanf("%s",a)!=EOF)
+    {
+        printf("%d\n",lastdigit(a));
+    }
+    return 0;
+}
